b52854a614ec1fd47d428fc66cece7e66840b01b
DivineCats <castillocedric9@gmail.com>
2025-02-23 03:35:01 +0800

    Footer / Header

--- src/user-side.tsx
+++ src/user-side.tsx
@@ -1,13 +1,11 @@
-import Footer from "./footer";
-import Header from "./header";
 import HomePage from "./homepage/homepage";
+import Header from './header';
+import Footer from './footer';
 
 function UserSide(){
     return (
         <div>
-            <div>
                 <Header />
-            </div>
             <div>
                 <HomePage />
             </div>
