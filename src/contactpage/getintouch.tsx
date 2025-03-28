1ad25d97bebbbd562c7a46c16fa86f8c93c491ca
France Angelo Joel M. Reyes <134769738+afgeloo@users.noreply.github.com>
2025-03-29 00:15:44 +0800

    fixed other contact infos database

--- src/contactpage/getintouch.tsx
+++ src/contactpage/getintouch.tsx
@@ -1,57 +1,88 @@
 import "./css/getintouch.css";
+import { useEffect, useState } from "react";
 
-function GetInTouch(){
-    return(
-        <div className="getintouch-sec">
-            <div className="getintouch-content">
-                <h1 className="getintouch-header">Get in Touch</h1>
-                <p className="getintouch-description">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                </p>
+function GetInTouch() {
+  const [contactNo, setContactNo] = useState<string>("Loading...");
+  const [email, setEmail] = useState<string>("Loading...");
+
+  useEffect(() => {
+    fetch("http://localhost/tara-kabataan-webapp/backend/api/aboutus.php")
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.contact_no) setContactNo(data.contact_no);
+        if (data.about_email) setEmail(data.about_email);
+      })
+      .catch((err) => {
+        console.error("Error fetching contact info:", err);
+        setContactNo("Unavailable");
+        setEmail("Unavailable");
+      });
+  }, []);
+
+  return (
+    <div className="getintouch-sec">
+      <div className="getintouch-content">
+        <h1 className="getintouch-header">Get in Touch</h1>
+        <p className="getintouch-description">
+          Reach out to us through any of our contact points below. We're here to listen and connect with you!
+        </p>
+      </div>
+
+      <div className="getintouch-sub-sec">
+        {/* Left Contact Info */}
+        <div className="getintouch-left">
+          <div className="contact-telephone">
+            <div className="contact-telephone-icon">
+              <img
+                src="./src/assets/contactpage/telephone.png"
+                alt="Telephone Icon"
+                draggable="false"
+              />
             </div>
-            <div className="getintouch-sub-sec">
-                <div className="getintouch-left">
-                    <div className="contact-telephone">
-                        <div className="contact-telephone-icon">
-                            <img src="./src/assets/contactpage/telephone.png" alt="Telephone Icon" />
-                        </div>
-                        <div className="contact-telephone-details">
-                            <h1 className="contact-telephone-header">Telephone</h1>
-                            <p className="contact-telephone-no">
-                                8888-1234
-                            </p>
-                        </div>
-                    </div>
-                    <div className="contact-email">
-                        <div className="contact-email-icon">
-                            <img src="./src/assets/contactpage/email.png" alt="Telephone Icon" />
-                        </div>
-                        <div className="contact-email-details">
-                            <h1 className="contact-email-header">Email</h1>
-                            <p className="contact-email">
-                                tarakabataan@gmail.com
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div className="getintouch-right">
-                    <h1 className="getintouch-right">Social Media</h1>
-                    <div className="contact-facebook">
-                        <img src="./src/assets/contactpage/facebook.png" alt="Telephone Icon" />
-                        <p>
-                            Tara Kabataan
-                        </p>
-                    </div>
-                    <div className="contact-instagram">
-                        <img src="./src/assets/contactpage/instargram.png" alt="Telephone Icon" />
-                        <p>
-                            Tara Kabataan
-                        </p>
-                    </div>
-                </div>
+            <div className="contact-telephone-details">
+              <h1 className="contact-telephone-header">Telephone</h1>
+              <p className="contact-telephone-no">{contactNo}</p>
             </div>
+          </div>
+
+          <div className="contact-email">
+            <div className="contact-email-icon">
+              <img
+                src="./src/assets/contactpage/email.png"
+                alt="Email Icon"
+                draggable="false"
+              />
+            </div>
+            <div className="contact-email-details">
+              <h1 className="contact-email-header">Email</h1>
+              <p className="contact-email">{email}</p>
+            </div>
+          </div>
         </div>
-    );
+
+        {/* Right Socials */}
+        <div className="getintouch-right">
+          <h1 className="getintouch-right-header">Social Media</h1>
+          <div className="contact-facebook">
+            <img
+              src="./src/assets/contactpage/facebook.png"
+              alt="Facebook Icon"
+              draggable="false"
+            />
+            <p>Tara Kabataan</p>
+          </div>
+          <div className="contact-instagram">
+            <img
+              src="./src/assets/contactpage/instargram.png"
+              alt="Instagram Icon"
+              draggable="false"
+            />
+            <p>@tarakabataan</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default GetInTouch;
